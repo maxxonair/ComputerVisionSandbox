7700f5c88febebea4f0b75697878e28ce9d2fa16
maxxonair <maxx2x@gmx.de>
2022-01-15 15:22:13 +0000

    Bug fixes for camera calibration

--- 01_StereoVision/cameraCalibration_v2.py
+++ 01_StereoVision/cameraCalibration_v2.py
@@ -1,39 +1,70 @@
+# -------------------------------------------------------------------------------------------
+#               >> Camera calibration 
+# -------------------------------------------------------------------------------------------
+# 
+# From:
+# https://docs.opencv.org/3.4/dc/dbb/tutorial_py_calibration.html
+# 
+# Setup:
+# 
+# So to find pattern in chess board, we can use the function, cv.findChessboardCorners(). 
+# We also need to pass what kind of pattern we are looking for, like 8x8 grid, 5x5 grid etc. 
+# In this example, we use 7x6 grid. (Normally a chess board has 8x8 squares and 7x7 internal 
+# corners). It returns the corner points and retval which will be True if pattern is obtained. 
+# These corners will be placed in an order (from left-to-right, top-to-bottom)
+# 
+# 
+# -------------------------------------------------------------------------------------------
 import numpy as np
+import os
 import cv2 as cv
 import glob
 from tqdm import tqdm
-
+# -------------------------------------------------------------------------------------------
+#               >> Settings
+# -------------------------------------------------------------------------------------------
 setDrawPlot     = False
 savePlot        = True
 
 #Define size of chessboard target.
-chessboard_size = (8,6)
+boardSize = (8,5)
 
 # Process images at scale: 
-scale_percent = 35
+scale_percent = 50
 
 # termination criteria
 criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-
+# -------------------------------------------------------------------------------------------
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((6*7,3), np.float32)
-objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
+objp = np.zeros((boardSize[1]*boardSize[0],3), np.float32)
+objp[:,:2] = np.mgrid[0:boardSize[0],0:boardSize[1]].T.reshape(-1,2)
 
 # Arrays to store object points and image points from all the images.
 objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane.
 
 # Flag to inicate that corners have been found 
-isSuccess = False
+isSuccess       = False
+successCounter  = 0 
+imageIndex      = 0 
 
 # Set file paths: 
 parameterFilePath   = "./03_camera_parameters/"
 processedImagePath  = "./02_processed_calibration_images/"
 inputFilePath       = "./01_calibration_images/"
 
+# List calibration images:
 images = glob.glob(inputFilePath+"*")
 
-imageIndex = 0 
+def cleanFolder(folderPath):
+    files = glob.glob(folderPath+'*')
+    for f in files:
+        os.remove(f)
+
+# Empty folder for processed images 
+cleanFolder(processedImagePath)
+# Empty folder for calibration parameters:
+cleanFolder(parameterFilePath)
 
 for fname in tqdm(images):
     # Load image 
@@ -54,29 +85,33 @@ for fname in tqdm(images):
     print("Image loaded, Analizying...")
 
     # Find the chess board corners
-    ret, corners = cv.findChessboardCorners(outputImage, chessboard_size, None)
+    ret, corners = cv.findChessboardCorners(outputImage, boardSize, None)
 
     # If found, add object points, image points (after refining them)
     if ret == True:
         objpoints.append(objp)
-        corners2    = cv.cornerSubPix(outputImage,corners, (11,11), (-1,-1), criteria)
         imgpoints.append(corners)
+        corners2    = cv.cornerSubPix(outputImage,corners, (11,11), (-1,-1), criteria)
         isSuccess   =  True
-        # Draw and display the corners
+        successCounter = successCounter + 1
+        # Save scaled images with drawn corners
         if savePlot:
-            cv.drawChessboardCorners(outputColor, chessboard_size, corners2, ret)
+            cv.drawChessboardCorners(outputColor, boardSize, corners2, ret)
             savePath = processedImagePath + fileName
             print(savePath)
             cv.imwrite(savePath, outputColor)
+            # Draw and display the corners
         if setDrawPlot:
-            cv.drawChessboardCorners(outputColor, chessboard_size, corners2, ret)
+            cv.drawChessboardCorners(outputColor, boardSize, corners2, ret)
             cv.imshow('img', outputColor)
             cv.waitKey(500)
     else:
+        print("")
         print(" >> ("+str(imageIndex)+")  No chessboard found.")
+        print("")
     imageIndex = imageIndex + 1
 
-            
+print(str(successCounter) + " Images processed. "+str(successCounter/imageIndex * 100)+" percent. ")           
 #cv.destroyAllWindows()   
 
 if isSuccess:
